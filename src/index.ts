d47ee6f55d7671584f269ae52e6e1c6e329a918b
Paulo Junior <paulojpjunior@gmail.com>
2024-09-04 14:03:18 -0300

    food-backend
    Instalação e configuração do Express.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world!!!')
+import exp from "constants";
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+
+dotenv.config();
+
+const app: Express = express(); //o express é tipo um mini servidor.
+const port = process.env.PORT || 5000; //config. da porta.
+
+app.use(express.json()); //config. o express para o tipo json.
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world!!!!!");
+});
+
+app.listen(port, () => {   //mostrando para o express em qual porta o projeto está.
+  console.log(`Server running on port ${port}`);
+});
