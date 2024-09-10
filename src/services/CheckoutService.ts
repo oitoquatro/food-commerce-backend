c7b2b5141c1993cf0beb8f91cbd63c833c708d28
Paulo Junior <paulojpjunior@gmail.com>
2024-09-09 21:12:55 -0300

    food-backend
    Processando Customer em CheckoutService.

--- src/services/CheckoutService.ts
+++ src/services/CheckoutService.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { Customer, PrismaClient } from "@prisma/client";
 import { CustomerData } from "../interfaces/CustomerData";
 import { PaymentData } from "../interfaces/PaymentData";
 import { SnackData } from "../interfaces/SnackData";
@@ -35,9 +35,24 @@ export default class CheckoutService {
         cart.find((item) => item.id === snack.id)?.quantity! *
         Number(snack.price),
     }));
-    console.log(`snacksInCart`, snacksInCart);
+    //console.log(`snacksInCart`, snacksInCart);
     // TODO: "registrar" os dados do cliente no BD
+    const customerCreated = await this.createCustomer(customer)
+    console.log(`customerCreated`, customerCreated)
     // TODO: criar uma order
     // TODO: processar o pagamento
+  }
+
+  private async createCustomer(customer: CustomerData): Promise<Customer> {
+    const customerCreated = await this.prisma.customer.upsert({
+      //onde o email solicitado for válido faz o update (atualiza) senão tem que criar um.
+      where: {
+        email: customer.email,
+      },
+      update: customer,
+      create: customer,
+    });
+
+    return customerCreated
   }
 }
